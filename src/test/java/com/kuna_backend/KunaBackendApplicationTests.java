2397b3b4a135f28051f82eea61bcb79c56491998
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2022-11-30 19:09:18 +0100

    First commit

---
+++ src/test/java/com/kuna_backend/KunaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuna_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
