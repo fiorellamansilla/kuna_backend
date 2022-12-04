8f310b22ed66a5a1503d38b488ff07c543e3ce74
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2022-12-04 18:56:27 +0100

    User Controller and Dao Service created

---
+++ src/main/java/com/kuna_backend/dao/UserDaoService.java
@@ -0,0 +1,52 @@
+package com.kuna_backend.dao;
+
+import com.kuna_backend.entities.Order;
+import com.kuna_backend.entities.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 0;
+
+    static {
+        users.add(new User(++usersCount,
+                "Isabel",
+                "123",
+                "isabel@gmail.com",
+                "Peru",
+                Boolean.FALSE,
+                Boolean.TRUE,
+                LocalDateTime.now()));
+    };
+
+    // GET all users Method
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+    // Get one User by ID Method
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    // Delete User by Id Method
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
+}
