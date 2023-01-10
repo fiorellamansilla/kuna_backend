59d4bcdadabcc4946b23a8cb145dd028e293327d
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-01-10 20:04:01 +0100

    Data Source Configuration implemented

---
+++ src/main/java/com/kuna_backend/config/DatasourceConfig.java
@@ -0,0 +1,23 @@
+package com.kuna_backend.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+}
