658bf8a4e77645c1e2b1f986b8d9a88a9d110c32
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-01-20 17:09:37 +0100

    Improved style of Client Controller

--- src/main/java/com/kuna_backend/controllers/ClientController.java
+++ src/main/java/com/kuna_backend/controllers/ClientController.java
@@ -2,6 +2,7 @@ package com.kuna_backend.controllers;
 
 import com.kuna_backend.entities.Client;
 import com.kuna_backend.services.ClientService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
