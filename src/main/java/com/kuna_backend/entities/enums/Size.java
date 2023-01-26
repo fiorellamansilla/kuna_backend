203994e6f9a2d17a9919b7301b0170d32e6b2bee
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-01-26 16:26:03 +0100

    Enums of Item modified

--- src/main/java/com/kuna_backend/entities/enums/Size.java
+++ src/main/java/com/kuna_backend/entities/enums/Size.java
@@ -1,10 +1,7 @@
 package com.kuna_backend.entities.enums;
 
 public enum Size {
-    NEWBORN("0 - 3m"),
-    BABY_FIRST("3 - 6m"),
-    BABY_SECOND("6 - 12m");
-
-    Size(String s) {
-    }
+    NEWBORN,
+    BABY,
+    TODDLER
 }
