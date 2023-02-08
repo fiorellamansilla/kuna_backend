087dd8ac4200a671a8198db1404f8821d301cc36
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-02-08 21:04:22 +0100

    Last commit

--- src/main/java/com/kuna_backend/entities/Client.java
+++ src/main/java/com/kuna_backend/entities/Client.java
@@ -1,6 +1,5 @@
 package com.kuna_backend.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
