f8fd15859eeee6c08cb80aa72ecae8b3673fe633
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-02-09 16:27:04 +0100

    Jakarta persistence imports modified in Client Entity

--- src/main/java/com/kuna_backend/entities/Client.java
+++ src/main/java/com/kuna_backend/entities/Client.java
@@ -1,6 +1,14 @@
 package com.kuna_backend.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.FetchType;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
