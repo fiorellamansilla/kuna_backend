c2c8394d619d12496cf4fc599aebb0c6e603afc7
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-02-15 17:24:43 +0100

    Made comment

--- src/main/java/com/kuna_backend/controllers/CheckoutController.java
+++ src/main/java/com/kuna_backend/controllers/CheckoutController.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// Prepares the model with the necessary information that the checkout form from the Frontend would need //
 @Controller
 public class CheckoutController {
 
@@ -17,7 +17,7 @@ public class CheckoutController {
     //  http://localhost:8080/checkout?amount=10  //
     @GetMapping("/checkout")
     public String checkout(Model model, @RequestParam(required = true) Integer amount) {
-        model.addAttribute("amount", amount*100);
+        model.addAttribute("amount", amount*100); //cents according to Stripe.
         model.addAttribute("stripePublicKey", stripePublicKey);
         model.addAttribute("currency", Currency.EUR);
         return "checkout";
