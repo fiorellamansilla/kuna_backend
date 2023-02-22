dbf2bc21ab602a842ed1cc3746ab020ccd84f462
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-02-22 10:35:15 +0100

    Category enum created

---
+++ src/main/java/com/kuna_backend/models/enums/Category.java
@@ -0,0 +1,14 @@
+package com.kuna_backend.models.enums;
+
+public enum Category {
+    BODY,
+    SHORT,
+    SHIRT,
+    DRESS,
+    PANTS,
+    ROMPER,
+    SWEATER,
+    OUTFIT,
+    PYJAMA,
+    ACCESSORY
+}
