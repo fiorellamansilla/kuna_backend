6f17735bdd19b3ae66bced50f113dea5c6409a09
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-03-03 17:39:47 +0100

    Implemented Signup Method in Client Service

--- src/main/java/com/kuna_backend/services/ClientService.java
+++ src/main/java/com/kuna_backend/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.kuna_backend.services;
 
+import com.kuna_backend.dtos.ResponseDto;
+import com.kuna_backend.dtos.SignupDto;
 import com.kuna_backend.models.Client;
 import com.kuna_backend.repositories.ClientRepository;
 import jakarta.transaction.Transactional;
@@ -28,6 +30,12 @@ public class ClientService {
 
     public void deleteClient (Integer id) {
         clientRepository.deleteById(id);
+    }
+
+    public ResponseDto signUp(SignupDto signupDto) {
+        ResponseDto responseDto = new ResponseDto("success", "test response");
+        return responseDto;
+
     }
 
 }
