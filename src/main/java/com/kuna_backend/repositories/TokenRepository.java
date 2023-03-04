d11149958a837f2d499684d156d236497f65a0ac
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-03-04 11:46:34 +0100

    get Token method implemented in Authentication Service

--- src/main/java/com/kuna_backend/repositories/TokenRepository.java
+++ src/main/java/com/kuna_backend/repositories/TokenRepository.java
@@ -1,9 +1,11 @@
 package com.kuna_backend.repositories;
 
 import com.kuna_backend.models.AuthenticationToken;
+import com.kuna_backend.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TokenRepository extends JpaRepository <AuthenticationToken, Integer> {
+    AuthenticationToken findByClient(Client client);
 }
--- src/main/java/com/kuna_backend/services/AuthenticationService.java
+++ src/main/java/com/kuna_backend/services/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.kuna_backend.services;
 
 import com.kuna_backend.models.AuthenticationToken;
+import com.kuna_backend.models.Client;
 import com.kuna_backend.repositories.TokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,9 @@ public class AuthenticationService {
     TokenRepository tokenRepository;
     public void saveConfirmationToken(AuthenticationToken authenticationToken) {
         tokenRepository.save(authenticationToken);
+    }
+
+    public AuthenticationToken getToken(Client client) {
+        return tokenRepository.findByClient(client);
     }
 }
