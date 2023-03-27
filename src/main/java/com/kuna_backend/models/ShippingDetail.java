cf3fe7723b1d0efc32754638ae43a0e599137508
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-03-27 17:57:52 +0200

    Shipping detail model created

---
+++ src/main/java/com/kuna_backend/models/ShippingDetail.java
@@ -0,0 +1,137 @@
+package com.kuna_backend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.JoinColumn;;import java.util.Date;
+
+@Entity
+@Table(name = "shipping_detail")
+public class ShippingDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "address", length = 128, nullable = false)
+    private String address;
+
+    @Column(name = "zip_code", length = 64, nullable = false)
+    private String zipCode;
+
+    @Column(name = "city", length = 32, nullable = false)
+    private String city;
+
+    @Column(name = "country", length = 32, nullable = false)
+    private String country;
+
+    @Column(name = "phone", length = 32, nullable = false)
+    private String phone;
+
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    // One-to-One relationship with Client //
+    @OneToOne
+    @JoinColumn (name = "client_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Client client;
+
+    // One-to-One relationship with Order //
+    @OneToOne
+    @JoinColumn (name = "order_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Order order;
+
+    public ShippingDetail() {
+    }
+
+    public ShippingDetail(Integer id, String address, String zipCode, String city, String country, String phone, Date createdAt, Client client, Order order) {
+        this.id = id;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.country = country;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.client = client;
+        this.order = order;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
