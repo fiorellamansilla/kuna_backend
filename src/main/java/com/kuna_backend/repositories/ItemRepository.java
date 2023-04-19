5b9c9f357bd296650c1130dfd9d2c47a2b5d638a
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-04-19 18:58:09 +0200

    Refactored item repository to product repository

--- src/main/java/com/kuna_backend/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kuna_backend.repositories;
-
-import com.kuna_backend.models.Item;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ItemRepository extends CrudRepository <Item, Integer> {
-}
