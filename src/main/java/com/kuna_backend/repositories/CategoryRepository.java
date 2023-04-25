c02998f99090b2eaf09d9881bfbc21c77296680c
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-04-25 11:39:03 +0200

    Category repository created

---
+++ src/main/java/com/kuna_backend/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.kuna_backend.repositories;
+
+import com.kuna_backend.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByCategoryName(String categoryName);
+
+}
