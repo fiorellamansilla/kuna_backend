4ae5625abe99409f7ec7ef729be7945cdccba675
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-05-04 09:21:40 +0200

    Product Variation Service with methods for create and get implemented

--- src/main/java/com/kuna_backend/services/ProductVariationService.java
+++ src/main/java/com/kuna_backend/services/ProductVariationService.java
@@ -1,13 +1,16 @@
 package com.kuna_backend.services;
 
+import com.kuna_backend.dtos.product.ProductVariationDto;
 import com.kuna_backend.exceptions.ProductNotExistsException;
 import com.kuna_backend.models.Product;
 import com.kuna_backend.models.ProductVariation;
 import com.kuna_backend.repositories.ProductVariationRepository;
+
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,12 +20,36 @@ public class ProductVariationService {
     @Autowired
     private ProductVariationRepository productVariationRepository;
 
+    public static ProductVariationDto getDtoFromProductVariation(ProductVariation productVariation) {
+        ProductVariationDto productVariationDto = new ProductVariationDto(productVariation);
+        return productVariationDto;
+    }
+
+    public static ProductVariation getProductVariationFromDto(ProductVariationDto productVariationDto, Product product) {
+        ProductVariation productVariation = new ProductVariation(productVariationDto, product);
+        return productVariation;
+    }
+
+    // Create Product Variation - method //
+    public void createProductVariation (ProductVariationDto productVariationDto, Product product) {
+        ProductVariation productVariation = getProductVariationFromDto(productVariationDto, product);
+        productVariationRepository.save(productVariation);
+    }
+
+    public List<ProductVariation> getAllProductVariations() {
+        return (List<ProductVariation>) productVariationRepository.findAll();
+    }
+
     public ProductVariation getProductVariationById(Integer productVariationId) throws ProductNotExistsException {
-        Optional<Product> optionalProductVariation= productVariationRepository.findById(productVariationId);
+        Optional<ProductVariation> optionalProductVariation = productVariationRepository.findById(productVariationId);
         // Check if the product variation exists
         if (optionalProductVariation.isEmpty()) {
-            throw new ProductNotExistsException("Product id is invalid:" + productVariationId);
+            throw new ProductNotExistsException("Product Variation id is invalid:" + productVariationId);
         }
-        return (ProductVariation) optionalProductVariation.get().getProductVariations();
+        return optionalProductVariation.get() ;
+    }
+
+    public void deleteProductVariation (Integer id) {
+        productVariationRepository.deleteById(id);
     }
 }
