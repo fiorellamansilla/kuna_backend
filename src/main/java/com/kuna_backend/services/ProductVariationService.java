34fb493f1aed32cfcb3d75417f6277cec5b658f4
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-05-06 09:21:40 +0200

    Last commit

--- src/main/java/com/kuna_backend/services/ProductVariationService.java
+++ src/main/java/com/kuna_backend/services/ProductVariationService.java
@@ -8,6 +8,9 @@ import com.kuna_backend.repositories.ProductVariationRepository;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
