c6ee72cc84affc47f237802b74557ecb74780df4
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-06-05 16:11:33 +0200

    Refactored create Session and Save Payment Unit tests

--- src/test/java/com/kuna_backend/PaymentServiceTest.java
+++ src/test/java/com/kuna_backend/PaymentServiceTest.java
@@ -14,22 +14,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class PaymentServiceTest {
     @Mock
     private PaymentRepository paymentRepository;
@@ -37,16 +43,14 @@ public class PaymentServiceTest {
     @Mock
     public Session session;
 
-    @Value("${STRIPE_SECRET_KEY}")
-    public String apiKey;
-
     @InjectMocks
     private PaymentService paymentService;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        Stripe.apiKey = apiKey;
+        Stripe.apiKey = "sk_test_51MaxUkEyNC1M1bEEpJcQrzYAcde8ST7cCT4NqohGmnyPz6OWsWQxWFbcUFbX2pytE5JjVeopRcBeh8RxPObdsmcz009C6OlGHh";
+        paymentService.baseURL = "http://localhost:8080/";
     }
 
     @Test
@@ -82,91 +86,80 @@ public class PaymentServiceTest {
     @Test
     public void createSession_ShouldCreateSessionSuccessfully() throws StripeException {
 
-        CheckoutItemDto checkoutItemDto = new CheckoutItemDto();
-        checkoutItemDto.setProductName("Product 1");
-        checkoutItemDto.setPrice(10.0);
-        checkoutItemDto.setQuantity(2);
-
-        String successURL = "http://localhost:8080/payment/success";
-        String failureURL = "http://localhost:8080/payment/failed";
-
-        // Mock Price Data Builder
-        SessionCreateParams.LineItem.PriceData.Builder priceDataBuilder =
-                mock(SessionCreateParams.LineItem.PriceData.Builder.class);
-        when(priceDataBuilder.setCurrency("usd")).thenReturn(priceDataBuilder);
-        when(priceDataBuilder.setUnitAmount(1000L)).thenReturn(priceDataBuilder);
-
-        SessionCreateParams.LineItem.PriceData priceData = mock(SessionCreateParams.LineItem.PriceData.class);
-        when(priceDataBuilder.build()).thenReturn(priceData);
-
-        // Mock Session Line Item Builder
-        SessionCreateParams.LineItem.Builder sessionLineItemBuilder =
-                mock(SessionCreateParams.LineItem.Builder.class);
-        when(sessionLineItemBuilder.setPriceData(priceData)).thenReturn(sessionLineItemBuilder);
-        when(sessionLineItemBuilder.setQuantity(2L)).thenReturn(sessionLineItemBuilder);
-
-        SessionCreateParams.LineItem sessionLineItem = mock(SessionCreateParams.LineItem.class);
-        when(sessionLineItemBuilder.build()).thenReturn(sessionLineItem);
+        String successURL = paymentService.baseURL + "payment/success";
+        String failureURL = paymentService.baseURL + "payment/failed";
 
         SessionCreateParams.Builder paramsBuilder = mock(SessionCreateParams.Builder.class);
-        when(paramsBuilder.addPaymentMethodType(any())).thenReturn(paramsBuilder);
-        when(paramsBuilder.setMode(any())).thenReturn(paramsBuilder);
-        when(paramsBuilder.setCancelUrl(any())).thenReturn(paramsBuilder);
-        when(paramsBuilder.addAllLineItem(any())).thenReturn(paramsBuilder);
-        when(paramsBuilder.setSuccessUrl(any())).thenReturn(paramsBuilder);
-
         SessionCreateParams params = mock(SessionCreateParams.class);
-        when(paramsBuilder.build()).thenReturn(params);
+        Session session = mock(Session.class);
+        List<CheckoutItemDto> checkoutItemDtoList = new ArrayList<>();
 
-        try (MockedStatic<SessionCreateParams.LineItem.PriceData> priceDataBuilderMock =
-                     mockStatic(SessionCreateParams.LineItem.PriceData.class);
-             MockedStatic<SessionCreateParams.LineItem> lineItemBuilderMock =
-                     mockStatic(SessionCreateParams.LineItem.class);
-             MockedStatic<SessionCreateParams> sessionBuilderMock =
-                     mockStatic(SessionCreateParams.class)) {
+        // Mock the static methods
+        try (MockedStatic<SessionCreateParams> sessionBuilderMock = mockStatic(SessionCreateParams.class);
+             MockedStatic<Session> sessionMock = mockStatic(Session.class);
+             MockedConstruction<SessionCreateParams.LineItem> lineItemConstruction = mockConstruction(SessionCreateParams.LineItem.class)) {
 
-            priceDataBuilderMock.when(SessionCreateParams.LineItem.PriceData::builder).thenReturn(priceDataBuilder);
-            lineItemBuilderMock.when(SessionCreateParams.LineItem::builder).thenReturn(sessionLineItemBuilder);
-            sessionBuilderMock.when(SessionCreateParams::builder).thenReturn(paramsBuilder);
+            // Stub the builder methods
+            when(SessionCreateParams.builder()).thenReturn(paramsBuilder);
+            when(paramsBuilder.addPaymentMethodType(any())).thenReturn(paramsBuilder);
+            when(paramsBuilder.setMode(any())).thenReturn(paramsBuilder);
+            when(paramsBuilder.setCancelUrl(eq(failureURL))).thenReturn(paramsBuilder);
+            when(paramsBuilder.setSuccessUrl(eq(successURL))).thenReturn(paramsBuilder);
+            when(paramsBuilder.addAllLineItem(any())).thenReturn(paramsBuilder);
+            when(paramsBuilder.build()).thenReturn(params);
 
-            when(paramsBuilder.setSuccessUrl(successURL)).thenReturn(paramsBuilder);
-            when(Session.create(params)).thenReturn(session);
+            // Stub the Session.create method
+            sessionMock.when(() -> Session.create(params)).thenReturn(session);
 
-            Session result = paymentService.createSession(List.of(checkoutItemDto));
+            Session result = paymentService.createSession(checkoutItemDtoList);
 
             assertEquals(session, result);
-            verify(paymentService).createPriceData(checkoutItemDto);
-            verify(paymentService).createSessionLineItem(checkoutItemDto);
             verify(paramsBuilder).addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD);
             verify(paramsBuilder).setMode(SessionCreateParams.Mode.PAYMENT);
             verify(paramsBuilder).setCancelUrl(failureURL);
-            verify(paramsBuilder).addAllLineItem(List.of(sessionLineItem));
+            verify(paramsBuilder).addAllLineItem(any());
             verify(paramsBuilder).setSuccessUrl(successURL);
-            verify(session).create(params);
+            verify(paramsBuilder).build();
+
+            // Verify the static method call
+            sessionMock.verify(() -> Session.create(params));
+
         }
     }
 
     @Test
-    void savePaymentFromSession_ShouldSavePaymentSuccessfully() throws StripeException {
-        
-        String stripeToken = "test_token";
-        when(Session.retrieve(stripeToken)).thenReturn(session);
+    public void savePaymentFromSession_ShouldSavePaymentSuccessfully() throws StripeException {
 
-        when(session.getAmountTotal()).thenReturn(1000L);
-        when(session.getCurrency()).thenReturn("usd");
-        when(session.getId()).thenReturn("session_id");
-        when(session.getPaymentStatus()).thenReturn("paid");
-        when(session.getCreated()).thenReturn(System.currentTimeMillis() / 1000L);
+        String stripeToken = "cs_test_a14LmWmbRX68zuH7z45k2cosRfWoq5sTE1srkhuekLlzhHzgI1Hu6Dqh9M";
 
-        Client client = new Client();
+        // Mock the static method Session.retrieve
+        try (MockedStatic<Session> sessionMock = mockStatic(Session.class)) {
+            sessionMock.when(() -> Session.retrieve(stripeToken)).thenReturn(session);
+            when(session.getAmountTotal()).thenReturn(1000L);
+            when(session.getCurrency()).thenReturn("usd");
+            when(session.getId()).thenReturn(stripeToken);
+            when(session.getPaymentStatus()).thenReturn("paid");
+            when(session.getCreated()).thenReturn(System.currentTimeMillis() / 1000L);
 
-        paymentService.savePaymentFromSession(stripeToken, client);
+            Client clientMock = mock(Client.class);
 
-        verify(paymentRepository).save(any(Payment.class));
+            paymentService.savePaymentFromSession(stripeToken, clientMock);
+
+            // Verify that the paymentRepository.save method was called with the expected Payment object
+            verify(paymentRepository).save(argThat(payment ->
+                    payment.getAmount() == 10.0f &&
+                            payment.getCurrency().equals("USD") &&
+                            payment.getStripeToken().equals(stripeToken) &&
+                            payment.getPaymentStatus().equals("PAID") &&
+                            payment.getProvider().equals("Stripe") &&
+                            payment.getClient() == clientMock
+            ));
+        }
     }
 
     @Test
     public void testGetAllPayments() {
+
         List<Payment> paymentList = new ArrayList<>();
         when(paymentRepository.findAll()).thenReturn(paymentList);
 
@@ -178,6 +171,7 @@ public class PaymentServiceTest {
 
     @Test
     public void testGetPaymentById() {
+
         String stripeToken = "test_token";
         Payment payment = new Payment();
         when(paymentRepository.findPaymentByStripeToken(stripeToken)).thenReturn(payment);
@@ -186,5 +180,6 @@ public class PaymentServiceTest {
 
         assertEquals(payment, result);
         verify(paymentRepository).findPaymentByStripeToken(stripeToken);
+
     }
 }
