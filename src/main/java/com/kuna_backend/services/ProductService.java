d79abbc36da267d68af790cb5576b64d7fb982c6
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2023-10-16 15:56:38 +0200

    Implemented a method to create the Product Variation which is assigned to a specific Product when updating it

--- src/main/java/com/kuna_backend/services/ProductService.java
+++ src/main/java/com/kuna_backend/services/ProductService.java
@@ -1,11 +1,13 @@
 package com.kuna_backend.services;
 
 import com.kuna_backend.dtos.product.ProductDto;
+import com.kuna_backend.dtos.product.ProductVariationDto;
 import com.kuna_backend.exceptions.ProductNotExistsException;
 import com.kuna_backend.models.Category;
 import com.kuna_backend.models.Product;
+import com.kuna_backend.models.ProductVariation;
 import com.kuna_backend.repositories.ProductRepository;
-
+import com.kuna_backend.repositories.ProductVariationRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,7 +25,10 @@ import java.util.Optional;
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ProductVariationRepository productVariationRepository;
 
+    // PRODUCT
     public static ProductDto getDtoFromProduct(Product product) {
         ProductDto productDto = new ProductDto(product);
         return productDto;
@@ -62,6 +67,43 @@ public class ProductService {
         return optionalProduct.get();
     }
 
+    // PRODUCT VARIATIONS
+    public static ProductVariation getProductVariationFromDto(ProductVariationDto productVariationDto, Product product) {
+        ProductVariation productVariation = new ProductVariation(productVariationDto, product);
+        return productVariation;
+    }
+
+    public Product createProductVariationForProduct (Integer productId, ProductVariationDto productVariationDto) {
+
+        // Retrieve the specific Product based on the ID.
+        Product product = getProductById(productId);
+
+        // Create a new Product Variation entity and populate with the corresponding data from ProductVariationDto
+        ProductVariation productVariation = getProductVariationFromDto(productVariationDto, product);
+
+        // Set the Product to which the ProductVariation belongs
+        productVariation.setProduct(product);
+
+        // Save the new ProductVariation entity in the database, which will generate an unique ID for it.
+        productVariation = productVariationRepository.save(productVariation);
+
+        // Add the ProductVariation to the Product's collection
+        product.getProductVariations().add(productVariation);
+
+        // Save and return the updated Product
+        return productRepository.save(product);
+    }
+
+    //TODO: Implement an Update product variations from a Product method. This is necessary because so the stock can get easily updated.
+
+    public void updateProductVariation(Integer productVariationId, ProductVariationDto productVariationDto, Product product) {
+        ProductVariation productVariation = getProductVariationFromDto(productVariationDto, product);
+        productVariation.setId(productVariationId);
+        productVariationRepository.save(productVariation);
+    }
+
+    // TODO: Evaluate what to do with these methods from Product.
+    // TODO: Implement a method for Updating the Product attributes without the Variations. i.e the price.
     // Update a specific Product by ID method
     public void updateProduct(Integer productId, ProductDto productDto, Category category) {
         Product product = getProductFromDto(productDto, category);
