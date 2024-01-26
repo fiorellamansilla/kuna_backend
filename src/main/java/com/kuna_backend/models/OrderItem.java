aad6bf499e67048950edc41bc1734074e67b346f
Fiorella Mansilla <111742669+fiorellamansilla@users.noreply.github.com>
2024-01-26 15:45:32 +0100

    OrderItem : refactored ID primitive data type to Long wrapper class

--- src/main/java/com/kuna_backend/models/OrderItem.java
+++ src/main/java/com/kuna_backend/models/OrderItem.java
@@ -21,7 +21,7 @@ public class OrderItem {
     @Id
     @Column (name = "order_item_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column (name = "quantity", nullable = false)
     private int quantity;
