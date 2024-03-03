9d39652761ae04b78bbc5141d3f3c6e65cd4d7a7
Diego Mansilla <dmansilla07@gmail.com>
2024-03-03 18:16:34 -0300

    Add random data

---
+++ src/main/resources/db/migration/V13__Add_random_products.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (category_id, name_product, price, desc_product, image_url)
+VALUES (4, 'Baby clothing', 299.99, 'A stylish and comfortable baby clothing', 'http://example.com/images/elegant-sofa.jpg');
+
+INSERT INTO product_variation (product_id, size, color, quantity_stock)
+VALUES (5, 'BABY', 'WHITE', 10);
